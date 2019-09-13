618d4df0f49a3dbda9e85fcfe6162a846ef05bc6
Kaspar Manz <kaspar.manz@tamedia.ch>
2019-09-13 11:29:26 +0200

    fix: make everything ES5 compatible
    Because IE11 is being complicated.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-;(function (global, factory) {
+(function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined'
     ? factory(exports)
     : typeof define === 'function' && define.amd
@@ -7,12 +7,62 @@
 })(this, function (exports) {
   'use strict'
 
+  // Array.prototype.find polyfill – needed for IE11
+  // https://tc39.github.io/ecma262/#sec-array.prototype.find
+  if (!Array.prototype.find) {
+    Object.defineProperty(Array.prototype, 'find', {
+      value: function(predicate) {
+        // 1. Let O be ? ToObject(this value).
+        if (this == null) {
+          throw new TypeError('"this" is null or not defined');
+        }
+
+        var o = Object(this);
+
+        // 2. Let len be ? ToLength(? Get(O, "length")).
+        var len = o.length >>> 0;
+
+        // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+        if (typeof predicate !== 'function') {
+          throw new TypeError('predicate must be a function');
+        }
+
+        // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+        var thisArg = arguments[1];
+
+        // 5. Let k be 0.
+        var k = 0;
+
+        // 6. Repeat, while k < len
+        while (k < len) {
+          // a. Let Pk be ! ToString(k).
+          // b. Let kValue be ? Get(O, Pk).
+          // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+          // d. If testResult is true, return kValue.
+          var kValue = o[k];
+          if (predicate.call(thisArg, kValue, k, o)) {
+            return kValue;
+          }
+          // e. Increase k by 1.
+          k++;
+        }
+
+        // 7. Return undefined.
+        return undefined;
+      },
+      configurable: true,
+      writable: true
+    });
+  }
+
   /**
    * The available languages
    * @typedef {'de' | 'fr' | 'it' | 'ro'} LanguageCode
-   * Colors are defined as hex codes
+   */
+   /** Colors are defined as hex codes
    * @typedef {string} Color - A color as a hex code e.g. #123456
-   * Party IDs are numbers from 1 to about 50
+   */
+   /** Party IDs are numbers from 1 to about 50
    * @typedef {number} PartyId - The ID of a party as visible in the spreadsheet
    */
 
@@ -63,7 +113,9 @@
             ' is not available. Chose either "de", "fr", "it" or "ro"'
         )
       } else {
-        const party = colors.data.find(party => party['abbr_' + lang] === abbr)
+        // This method should be polyfilled, see above
+        // eslint-disable-next-line es5/no-es6-methods
+        var party = colors.data.find(function(party){return party['abbr_' + lang] === abbr})
         if (!party) {
           console.error(
             'Error in getParty: party ' +
@@ -89,34 +141,36 @@
   /**
    * Get the complete party definition by party id and language
    * @param {PartyId} id ID of the party in question
-   * @param {[LanguageCode='de']} lang Language that the return value is
+   * @param {LanguageCode} [lang='de'] Language that the return value is
    * @return {PartyDefinition}
    */
-  var getPartyById = (id, lang = 'de') => {
+  function getPartyById (id, lang) {
+    // Default value for language
+    lang = lang || 'de'
+
     if (available_languages.indexOf(lang) < 0) {
       throw new Error(
         'the requested language ' +
-          lang +
-          ' is not available. Chose either "de", "fr", "it" or "ro"'
-      )
+        lang +
+        ' is not available. Chose either "de", "fr", "it" or "ro"')
     } else {
-      var availableIds = colors.data.map(d => d.id)
-      if (!availableIds.includes(id)) {
+      var availableIds = colors.data.map(function (d) { return d.id })
+      if (availableIds.indexOf(id) < 0) {
         console.error(
           'the requested party id ' +
-            id +
-            ' is not available, returning fallback color'
-        )
+          id +
+          ' is not available, returning fallback color')
         return fallBackColor
       }
-      const party = colors.data.find(d => d.id === id)
+      // eslint-disable-next-line es5/no-es6-methods
+      var party = colors.data.find(function (d) { return d.id === id })
       return {
         abbr: party['abbr_' + lang],
         name: party['name_' + lang],
         color: party.color,
         blackOrWhite: party.on_color,
         fontColor: party.on_white,
-        fontColorOnBlack: party.on_black
+        fontColorOnBlack: party.on_black,
       }
     }
   }
@@ -128,7 +182,9 @@
    * @return {PartyDefinition}
    * @throws If the langauge that is requested is not valid, an Error is thrown
    */
-  var getParty = (lang, abbr) => getPartyFunctionForLanguage(lang)(abbr)
+  function getParty (lang, abbr) {
+    return getPartyFunctionForLanguage(lang)(abbr)
+  }
 
   // create one function per language for export
   var getPartyDE = getPartyFunctionForLanguage('de')
@@ -143,7 +199,7 @@
    * @return {string}
    */
   function getPartyAbbrById (id, lang) {
-    const party = getPartyById(id, lang)
+    var party = getPartyById(id, lang)
     if (party) return party.abbr
   }
 
@@ -154,7 +210,7 @@
    * @return {string} Name of the party in the lang specified
    */
   function getPartyNameById (id, lang) {
-    const party = getPartyById(id, lang)
+    var party = getPartyById(id, lang)
     if (party) return party.name
   }
 
